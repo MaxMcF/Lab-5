0e6a91ff3690a161b0878acf428d86fdbba0d1f9
MAX MCFARLAND <mmcfarland522@gmail.com>
2017-08-11 14:55:24 -0700

    Completed problem 3

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ Write a function called sum() that takes in two numbers as arguments and then re
 "The sum of 4 and 7 is 11."
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.
 For TODO item, be sure to change it to say DONE when you have successfully completed the task*/
-
+var multiplyAns = 0;
+var additionAns = 0;
 // TODO: Write your code here
 function sum(a,b){ //eslint-disable-line
   var result = a + b;
@@ -47,11 +48,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // TODO: Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-
+  var sumResult = sum(sum(a,b)[0],c)[0];
+  var prodResult = multiply(multiply(a,b)[0],c)[0];
+  var sumOutput = a + ' and ' + b + ' and ' + c + ' sum to ' + sumResult + '.';
+  var prodOutput = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + prodResult + '.';
+  return[sumResult, prodResult, sumOutput, prodOutput];
 }
 
 // TODO: Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
